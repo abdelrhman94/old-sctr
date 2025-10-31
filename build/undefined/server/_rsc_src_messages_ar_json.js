6b95359889177a9e8cde5fc70b1f727f55163505
Abdelrhman Saied Mohamed <abdelrhman.s.farag@gmail.com>
2025-10-31 21:06:41 +0300

    Added _rsc_src_messages_en_json.js

---
+++ build/undefined/server/_rsc_src_messages_ar_json.js
@@ -0,0 +1,25 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "_rsc_src_messages_ar_json";
+exports.ids = ["_rsc_src_messages_ar_json"];
+exports.modules = {
+
+/***/ "(rsc)/./src/messages/ar.json":
+/*!******************************!*\
+  !*** ./src/messages/ar.json ***!
+  \******************************/
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"Shared":{"sign_in_as":"Signed in as","profile":"Profile","settings":"Settings","logout":"Logout","language":"Language","dashboard":"Dashboard","studies":"Studies","requests":"Requests","loading":"Loading...","users":"Users"},"GlobalError":{"title":"Something went wrong!","description":"We encountered an unexpected error. Don\'t worry, our team has been notified.","tryAgain":"Try again","goHome":"Go back home"},"NotFoundPage":{"title":"Page Not Found","description":"Sorry, we couldn’t find the page you were looking for.","goHome":"Go back home"},"HomePage":{"title":"Hello world!","about":"Go to the about page"},"DashboardPage":{"title":"Dashboard","description":"This is the description of the dashboard page."},"RequestsPage":{"title":"Requests","description":"This is the description of the requests page."},"UsersPage":{"title":"Users","description":"This is the description of the users page.","add_new_user":"Create new user"},"StudiesPage":{"title":"Studies","description":"This is the description of the studies page.","add_new_study":"Add new study"},"CreateStudyPage":{"title":"Create New Study","description":"This is the description of the create new study page."},"SettingsPage":{"title":"Settings","description":"This is the description of the settings page."}}');
+
+/***/ })
+
+};
+;
